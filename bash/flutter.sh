34f5dbc5ffb520cdf3e84008995a48fd7329f80c
Vinh <thanh.vinh@hotmail.com>
2022-07-18 15:57:13 +0700

    Add flutter helper

---
+++ bash/flutter.sh
@@ -0,0 +1,58 @@
+# !/bin/bash
+
+###############################################################################
+# WIP
+###############################################################################
+
+root_dirs=$(cat <<EOF
+	assets assets/fonts assets/images assets/l10n
+	lib lib/src
+EOF
+)
+
+base_dirs=$(cat <<EOF
+	config
+	constants
+	models
+	providers
+	services
+	utils/helpers utils/mixins
+	widgets
+	modules
+EOF
+)
+
+feature_dirs=$(cat <<EOF
+	modules/feature-a
+	modules/feature-b
+EOF
+)
+
+function init_app() {
+	path=$1
+	# mkdir -p $root_dirs
+	for dir in $base_dirs; do
+		mkdir -p "lib/src/$dir"
+		echo '' > "lib/src/$dir/.gitkeep"
+	done
+
+	for dir in $feature_dirs; do
+		mkdir -p "lib/src/$dir"
+		for base_dir in $base_dirs; do
+			mkdir -p "lib/src/$dir/$base_dir"
+			echo '' > "lib/src/$dir/$base_dir/.gitkeep"
+		done
+	done
+}
+
+###############################################################################
+# New Flutter application
+###############################################################################
+function create_app() {
+	path=${1:-'app'}
+	flutter create ${path}
+	cd ${path}
+	init_app ${path}
+}
+
+create_app $1
