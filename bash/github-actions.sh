ee36671227fda959e53994593d59f56d8f670d08
Vinh <thanh.vinh@hotmail.com>
2022-10-06 17:01:42 +0700

    Update install script

--- bash/github-actions.sh
+++ bash/github-actions.sh
@@ -16,86 +16,6 @@ function github_tag_to_version() {
 }
 
 # -----------------------------------------------------------------------------
-# Required packages
-# -----------------------------------------------------------------------------
-function install_linux_packages() {
-	sudo apt install make jq unzip
-}
-
-function install_osx_packages() {
-	brew make install jq
-}
-
-# -----------------------------------------------------------------------------
-# systemd
-# -----------------------------------------------------------------------------
-function create_systemd_service() {
-	mkdir -p ~/.config/systemd/user/
-	pushd ~/.config/systemd/user/
-
-	# Create a simple service
-	echo '[Unit]' > actions-runner.service
-	echo 'Description=GitHub runner' >> actions-runner.service
-	echo >> actions-runner.service
-
-	echo '[Service]' >> actions-runner.service
-	echo 'Type=simple' >> actions-runner.service
-	echo 'Restart=always' >> actions-runner.service
-	echo 'RestartSec=5s' >> actions-runner.service
-	echo 'WorkingDirectory=/opt/actions-runner' >> actions-runner.service
-	echo 'ExecStart=/opt/actions-runner/run.sh' >> actions-runner.service
-	echo 'StandardOutput=syslog' >> actions-runner.service
-	echo 'StandardError=syslog' >> actions-runner.service
-	echo 'SyslogIdentifier=runner' >> actions-runner.service
-	echo >> actions-runner.service
-
-	echo '[Install]' >> actions-runner.service
-	echo 'WantedBy=multi-user.target' >> actions-runner.service
-
-	popd
-
-	# Register
-	systemctl --user daemon-reload
-	systemctl --user list-unit-files actions-runner.service
-	systemctl --user enable --now actions-runner.service
-	systemctl --user status actions-runner
-	# journalctl --user -f -u actions-runner
-}
-
-# -----------------------------------------------------------------------------
-# launchd
-# -----------------------------------------------------------------------------
-function create_launchd_daemon() {
-	pushd /Library/LaunchDaemons/
-
-	echo '<?xml version="1.0" encoding="UTF-8"?>' | sudo tee com.github.actions-runner.plist
-	echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' | sudo tee -a com.github.actions-runner.plist
-	echo '<plist version="1.0">' | sudo tee -a com.github.actions-runner.plist
-	echo '<dict>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<key>Label</key>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<string>com.github.actions.runner</string>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<key>ProgramArguments</key>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<array>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t\t<string>/opt/actions-runner/run.sh</string>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t</array>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<key>StandardOutPath</key>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<string>/tmp/actions-runner.stdout</string>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<key>StandardErrorPath</key>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<string>/tmp/actions-runner.stderr</string>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<key>RunAtLoad</key>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<true/>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<key>KeepAlive</key>' | sudo tee -a com.github.actions-runner.plist
-	echo '\t<true/>' | sudo tee -a com.github.actions-runner.plist
-	echo '</dict>' | sudo tee -a com.github.actions-runner.plist
-	echo '</plist>' | sudo tee -a com.github.actions-runner.plist
-
-	popd
-
-	sudo launchctl load -w /Library/LaunchDaemons/com.github.actions-runner.plist
-	sudo launchctl start com.github.actions.runner
-}
-
-# -----------------------------------------------------------------------------
 # Download
 # -----------------------------------------------------------------------------
 # Create a folder
@@ -132,12 +52,6 @@ rm actions-runner.tar.gz
 # -----------------------------------------------------------------------------
 # Startup
 # -----------------------------------------------------------------------------
-if [ "${os}" == "linux" ]; then
-	install_linux_packages
-	create_systemd_service
-elif [ "${os}" == "osx" ]; then
-	install_osx_packages
-	create_launchd_daemon
-elif [ "${os}" == "win" ]; then
-	echo 'Will be support windows services in the future'
-fi
+# Service
+./svc.sh install
+./svc.sh start
--- bash/install.sh
+++ bash/install.sh
@@ -6,22 +6,25 @@
 # Usage: ./install.sh [packages...]
 # -----------------------------------------------------------------------------
 
-distro=$(grep -oE '^ID=\w+' /etc/os-release | sed 's/ID=//')
-install_cmd='apt install -y'
+os_id=$(grep -oE '^ID=\w+' /etc/os-release | sed 's/ID=//')
+install_cmd=''
 bin_path='/usr/local/bin'
 
-if [ "$distro" = "alpine" ]; then
-  install_cmd='apk add --no-cache'
+if [ "$os_id" = "alpine" ]; then
+	install_cmd='apt install -y'
+elif [ "$os_id" = "alpine" ]; then
+  	install_cmd='apk add --no-cache'
+else
+	install_cmd='pacman -S'
 fi
 
-echo "distro=${distro}"
-echo "Install command=${install_cmd}"
+echo "OS ID: ${os_id}"
+echo "Install command: ${install_cmd}"
 
 # -----------------------------------------------------------------------------
 # The latest tag on GitHub
 # -----------------------------------------------------------------------------
 function get_github_latest_tag() {
-    # curl -s "https://api.github.com/repos/$1/tags" | grep -oP '"name": "\K[\w.]+' | head -n 1
 	curl -s "https://api.github.com/repos/$1/tags" | grep -oE '"name": ".*"' | head -n 1 | sed -e 's/[name :|"]//g'
 }
 
@@ -32,12 +35,13 @@ function github_tag_to_version() {
 # -----------------------------------------------------------------------------
 # Kubernetes
 # -----------------------------------------------------------------------------
-# kubectl
+# Kubectl
 function install_kubectl() {
-    curl -Lo ${bin_path}/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    chmod +x ${bin_path}/kubectl
+  curl -Lo ${bin_path}/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod +x ${bin_path}/kubectl
 }
 
+# Helm
 function install_helm() {
 	curl -s 'https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3' | sh
 }
@@ -51,12 +55,12 @@ function install_hugo() {
 	version=$(get_github_version ${tag})
 	echo "Version=${tag}"
 
-    curl -Lo hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/${tag}/hugo_extended_${version}_Linux-64bit.tar.gz"
-    mkdir -p hugo
-    tar -xf hugo.tar.gz -C hugo
-    chmod +x hugo/hugo
-    mv hugo/hugo ${bin_path}/
-    rm -rf hugo.tar.gz hugo/
+  curl -Lo hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/${tag}/hugo_extended_${version}_Linux-64bit.tar.gz"
+  mkdir -p hugo
+  tar -xf hugo.tar.gz -C hugo
+  chmod +x hugo/hugo
+  mv hugo/hugo ${bin_path}/
+  rm -rf hugo.tar.gz hugo/
 }
 
 # -----------------------------------------------------------------------------
@@ -79,13 +83,13 @@ function install_pg_partman() {
 	tag=$(get_github_latest_tag pgpartman/pg_partman)
 	version=$(github_tag_to_version ${tag})
 
-    curl -Lo pg_partman.tar.gz "https://github.com/pgpartman/pg_partman/archive/refs/tags/${tag}.tar.gz"
-    tar -x -f pg_partman.tar.gz
-    rm pg_partman.tar.gz
-    cd "pg_partman-${version}"
-    make && make install
-    cd ..
-    rm -rf "pg_partman-${version}"
+  curl -Lo pg_partman.tar.gz "https://github.com/pgpartman/pg_partman/archive/refs/tags/${tag}.tar.gz"
+  tar -x -f pg_partman.tar.gz
+  rm pg_partman.tar.gz
+  cd "pg_partman-${version}"
+  make && make install
+  cd ..
+  rm -rf "pg_partman-${version}"
 }
 
 function install_pg_cron() {
@@ -106,9 +110,9 @@ function install_typesense() {
 
 	echo "Version=${tag}"
 
-    curl -Lo typesense-server.tar.gz "https://dl.typesense.org/releases/${version}/typesense-server-${version}-linux-amd64.tar.gz"
-    tar -xf typesense-server.tar.gz
-    rm typesense-server.tar.gz
+  curl -Lo typesense-server.tar.gz "https://dl.typesense.org/releases/${version}/typesense-server-${version}-linux-amd64.tar.gz"
+  tar -xf typesense-server.tar.gz
+  rm typesense-server.tar.gz
 	chmod +x typesense-server
 	mv typesense-server ${bin_path}/
 }
@@ -117,29 +121,31 @@ function install_typesense() {
 # ERP
 # -----------------------------------------------------------------------------
 
-function install() {
-    if [ -z "$@" ]; then
-        echo "No packages specified"
-        exit 1
-    fi
 
-    for package in "$@"
-    do
-        echo "Installing ${package}"
-        case "${package}" in
-            kubectl)    install_kubectl;;
-			helm)		install_helm;;
-            hugo)       install_hugo;;
-			pg_jobmon)	install_pg_jobmon;;
-			pg_partman)	install_pg_partman;;
-			pg_cron)	install_pg_cron;;
-			typesense) 	install_typesense;;
-            *)
-                # "Use the distro install command"
-                ${install_cmd} ${package}
-                ;;
-        esac
-    done
+# -----------------------------------------------------------------------------
+function install() {
+  if [ -z "$@" ]; then
+    echo "No packages specified"
+    exit 1
+  fi
+
+  for package in "$@"
+  do
+    echo "Installing ${package}"
+    case "${package}" in
+    kubectl)    install_kubectl;;
+    helm)		    install_helm;;
+    hugo)       install_hugo;;
+    pg_jobmon)	install_pg_jobmon;;
+    pg_partman)	install_pg_partman;;
+    pg_cron)	  install_pg_cron;;
+    typesense) 	install_typesense;;
+    *)
+      # "Use the distro install command"
+      ${install_cmd} ${package}
+      ;;
+    esac
+  done
 }
 
 install "$@"
