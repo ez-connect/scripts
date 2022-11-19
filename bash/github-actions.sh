9785b59dcd64a605bd5b4ae9d6dbf52965000b04
Vinh <thanh.vinh@hotmail.com>
2022-11-19 23:48:21 +0700

    Update actions-runner working dir

--- bash/github-actions.sh
+++ bash/github-actions.sh
@@ -3,9 +3,9 @@
 # GitHub self-hosted runner
 # https://github.com/organizations/<name>/settings/actions/runners/new
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 # The latest tag on GitHub
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 function get_github_latest_tag() {
     # curl -s "https://api.github.com/repos/$1/tags" | grep -oP '"name": "\K[\w.]+' | head -n 1
 	curl -s "https://api.github.com/repos/$1/tags" | grep -oE '"name": ".*"' | head -n 1 | sed -e 's/[name :|"]//g'
@@ -15,13 +15,12 @@ function github_tag_to_version() {
 	echo $1 | sed 's/v//'
 }
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 # Download
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 # Create a folder
-sudo mkdir -p /opt/actions-runner
-sudo chmod a+rw /opt/actions-runner
-cd /opt/actions-runner
+sudo mkdir -p ~/actions-runner
+cd ~/actions-runner
 
 # Download the latest runner packagecreate_systemd_service
 read -p 'Which os (linux, osx, win) [linux]: ' input
@@ -34,24 +33,36 @@ version=$(github_tag_to_version ${tag})
 
 curl -o actions-runner.tar.gz -L "https://github.com/actions/runner/releases/download/${tag}/actions-runner-${os}-${architect}-${version}.tar.gz"
 
-# Optional: Validate the hash
-# echo "eb4e2fa6567d2222686be95ee23210e83fb6356dd0195149f96fd91398323d7f  actions-runner-linux-x64-2.297.0.tar.gz" | shasum -a 256 -c
-
 # Extract the installer
 tar xzf actions-runner.tar.gz
 rm actions-runner.tar.gz
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 # Configure
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 # Create the runner and start the configuration experience
 ./config.sh
 # Last step, run it!
 # ./run.sh
 
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
+# Install required packages
+# ---------------------------------------------------------
+package_management=apt
+if [ "${os}" == "osx" ]; then
+	package_management=brew
+elif [ "${os}" == "win" ]; then
+	package_management=scoop
+fi
+
+read -p "Which is your package management [${package_management}]: " input
+package_management=${input:-${package_management}}
+
+"${package_management} install make jq podman"
+
+# ---------------------------------------------------------
 # Startup
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------
 # Service
 ./svc.sh install
 ./svc.sh start
